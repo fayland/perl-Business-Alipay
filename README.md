86cd1d4bccc7551167778f90ad91d27416efd09e
Fayland Lam <fayland@gmail.com>
2013-09-14 13:35:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Business::Alipay - Blah blah blah
+
+# SYNOPSIS
+
+    use Business::Alipay;
+
+# DESCRIPTION
+
+Business::Alipay is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Business/Alipay.pm
@@ -0,0 +1,39 @@
+package Business::Alipay;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Business::Alipay - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Business::Alipay;
+
+=head1 DESCRIPTION
+
+Business::Alipay is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
